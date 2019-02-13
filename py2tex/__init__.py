7c0cfc9dd8171af52baba24ce796b6537eef2ac1
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2019-02-13 17:57:22 -0500

    add plot in init

--- py2tex/__init__.py
+++ py2tex/__init__.py
@@ -2,7 +2,9 @@ try:
     from tex_base import TexFile, TexEnvironment, build
     from document import Document
     from table import Table
+    from plot import Plot
 except:
     from .tex_base import TexFile, TexEnvironment, build
     from .document import Document
     from .table import Table
+    from .plot import Plot
