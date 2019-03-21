a53c4ea5671a542a0153bedd8c2c1c501d5d6147
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2019-03-21 14:45:39 -0400

    binding

--- py2tex/test_with_TikzPicture_wrapper.py
+++ py2tex/test_with_TikzPicture_wrapper.py
@@ -1,28 +1,45 @@
-from py2tex import TexEnvironment
+from functools import wraps
+from py2tex import TexEnvironment, TexCommand
 
 
-class Node:
+class Node(TexCommand):
+    """
+    This is a node.
+    """
     def __init__(self, label):
+        """
+        Args:
+            label (str): Label of the node.
+        """
         self.label = label
 
     def __repr__(self):
         return self.label
 
+    def some_func(self):
+        pass
+
 class Draw:
     pass
 
 
+
 class TikzPicture(TexEnvironment):
     def bind(self, *clss):
-        binded_cls = []
-        for cls in clss:
-            new_cls = type(f'Binded{cls.__name__}To{self.name}', (cls,), {})
-            def new_cls__new__(_cls, *a, **k):
-                instance = cls.__new__(_cls)
-                return self.new(instance)
-            new_cls.__new__ = new_cls__new__
-            binded_cls.append(new_cls)
-        return binded_cls[0] if len(binded_cls) == 1 else tuple(binded_cls)
+        binded_clss = tuple(self._bind(cls) for cls in clss)
+        return binded_clss[0] if len(binded_clss) == 1 else binded_clss
+
+    def _bind(self, cls_to_bind):
+        class BindedCls(cls_to_bind):
+            @wraps(cls_to_bind.__new__)
+            def __new__(cls, *args, **kwargs):
+                isinstance = cls_to_bind.__new__(cls)
+                return self.new(isinstance)
+        BindedCls.__name__ = 'Binded' + cls_to_bind.__name__
+        BindedCls.__qualname__ = 'Binded' + cls_to_bind.__qualname__
+        BindedCls.__doc__ = f"\tThis is a {cls_to_bind.__name__} object binded to {repr(self)}. Each time an instance is created, it is appended to the body of {repr(self)}. Everything else is identical.\n\n" + str(cls_to_bind.__doc__)
+        return BindedCls
+
 
 
 tikzpic = TikzPicture('Pic')
@@ -31,7 +48,8 @@ node, draw = tikzpic.bind(Node, Draw)
 a = node('hehe')
 b = node('haha')
 c = tikzpic.new(Node('hihi'))
+d = draw()
+Node('hoho')
+Node('hyhy')
 
 print(tikzpic.body)
-
-print(type(a))
