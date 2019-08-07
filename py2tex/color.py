b3ad5c06124daae14193d67182d769b791751ff7
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2019-08-06 20:00:10 -0400

    finished Color and tests

--- py2tex/color.py
+++ py2tex/color.py
@@ -1,21 +1,21 @@
 from py2tex import TexObject, TexCommand
 
 
+class DefineColor(TexCommand):
+    def __init__(self, color_name, *rgb):
+        super().__init__('definecolor', color_name, 'rgb', ','.join([str(c) for c in rgb]))
+
+
 class Color(TexObject):
     """
     """
     color_count = 0
-    def __init__(self, *rgb, color_name=''):
-        self.rgb = rgb
-        self.color_count += 1
-        if color_name:
-            self.color_name = color_name
-        else:
-            self.color_name = f'color{self.color_count}'
-        super().__init__()
+    def __init__(self, r, g, b, color_name=''):
+        super().__init__('color')
+        self.rgb = (r, g, b)
+        Color.color_count += 1
+        self.color_name = color_name or f'color{Color.color_count}'
+        self.add_to_preamble(DefineColor(self.color_name, *self.rgb))
 
     def build(self):
-        define_color = TexCommand('definecolor', self.color_name, 'rgb', *self.rgb)
-
-
-        return super().build()
+        return self.color_name
