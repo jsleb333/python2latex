9e4328f8e3d51aaa155a3fe9d495f0038c7a7df3
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2019-10-21 11:01:48 -0400

    removed unecessary try

--- py2tex/__init__.py
+++ py2tex/__init__.py
@@ -1,16 +1,7 @@
-try:
-    from tex_base import *
-    from tex_environment import TexEnvironment
-    from document import Document, Section, Subsection
-    from floating_environment import FloatingFigure, FloatingTable, FloatingEnvironmentMixin
-    from table import Table
-    from plot import Plot
-    from color import Color
-except:
-    from .tex_base import *
-    from .tex_environment import TexEnvironment
-    from .document import Document, Section, Subsection
-    from .floating_environment import FloatingFigure, FloatingTable, FloatingEnvironmentMixin
-    from .table import Table
-    from .plot import Plot
-    from .color import Color
+from .tex_base import *
+from .tex_environment import TexEnvironment
+from .document import Document, Section, Subsection
+from .floating_environment import FloatingFigure, FloatingTable, FloatingEnvironmentMixin
+from .table import Table
+from .plot import Plot
+from .color import Color
