9ea54abdbbc537935a28b30280c068b6b4154f5a
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2019-11-27 14:48:19 -0500

    add template to init

--- python2latex/__init__.py
+++ python2latex/__init__.py
@@ -5,3 +5,4 @@ from .floating_environment import FloatingFigure, FloatingTable, FloatingEnviron
 from .table import Table
 from .plot import Plot, LinePlot, MatrixPlot
 from .color import Color
+from .template import Template
