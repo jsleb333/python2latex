4b4def036be3d106ae7eb93d88af6345e25f3b6a
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2020-03-23 14:54:24 -0400

    bug fix in order of imports

--- python2latex/__init__.py
+++ python2latex/__init__.py
@@ -1,9 +1,8 @@
-from .color import Color
-from .document import Document, Section, Subsection
-from .floating_environment import FloatingFigure, FloatingTable, FloatingEnvironmentMixin
-from .plot import Plot, LinePlot, MatrixPlot
-from .color import Color
-from .template import Template
-from .table import Table
 from .tex_base import *
 from .tex_environment import TexEnvironment
+from .document import Document, Section, Subsection
+from .color import Color
+from .floating_environment import FloatingFigure, FloatingTable, FloatingEnvironmentMixin
+from .plot import Plot, LinePlot, MatrixPlot
+from .template import Template
+from .table import Table
