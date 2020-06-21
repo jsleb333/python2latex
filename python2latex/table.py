579f0270c45359d05c997144b8fa3e1318946afb
Frédérik Paradis <fredy_14@live.fr>
2020-06-21 11:51:15 -0400

    Add support for Real and Integral everywhere in Table

--- python2latex/table.py
+++ python2latex/table.py
@@ -1,4 +1,4 @@
-from numbers import Integral
+from numbers import Real, Integral
 import numpy as np
 
 from python2latex import FloatingTable, FloatingEnvironmentMixin
@@ -153,7 +153,7 @@ class Tabular(TexEnvironment):
 
     def __setitem__(self, idx, value):
         selected_area = self[idx]
-        if isinstance(value, (str, int, float)) and selected_area.size > 1:
+        if isinstance(value, (str, Real, Integral)) and selected_area.size > 1:
             # There are multirows or multicolumns to treat
             selected_area.multicell(value)
         else:
@@ -171,15 +171,15 @@ class Tabular(TexEnvironment):
     def _format_number(self, i, j, content):
         format_spec = self.formats_spec[i, j]
 
-        if not isinstance(content, (float, int)):
+        if not isinstance(content, (Real, Integral)):
             return content
 
         if format_spec is not None:
             content = format(content, format_spec)
-        elif format_spec is None and isinstance(content, float): # Fallback to default
-            content = format(content, self.float_format)
-        elif format_spec is None and isinstance(content, int):
+        elif format_spec is None and isinstance(content, Integral):
             content = format(content, self.int_format)
+        elif format_spec is None and isinstance(content, Real): # Fallback to default
+            content = format(content, self.float_format)
 
         if self.decimal_separator != '.':
             content = content.replace('.', self.decimal_separator)
@@ -405,14 +405,14 @@ class SelectedArea:
         best_idx = [(None, None)]
         for i, row in enumerate(self.data):
             for j, value in enumerate(row):
-                if isinstance(value, (float, int)) and value_is_better_than_best(value, best_value):
+                if isinstance(value, (Real, Integral)) and value_is_better_than_best(value, best_value):
                     best_idx = [(i, j)]
                     best_value = value
 
         # Find values close to best
         for i, row in enumerate(self.data):
             for j, value in enumerate(row):
-                if isinstance(value, (float, int)) and np.isclose(value, best_value, rtol, atol) \
+                if isinstance(value, (Real, Integral)) and np.isclose(value, best_value, rtol, atol) \
                     and (i, j) not in best_idx:
                     best_idx.append((i, j))
 
