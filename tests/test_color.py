f2a43f1c3e9e24d7cc7230e46617da40c356e2fe
Frédérik Paradis <fredy_14@live.fr>
2020-06-25 11:31:11 -0400

    Add textcolor class

--- tests/test_color.py
+++ tests/test_color.py
@@ -32,3 +32,57 @@ class TestColor:
             \begin{document}
             \somecommand{param}[color1]
             \end{document}''')
+
+
+
+class TestTextColor:
+    def teardown_method(self, mtd):
+        Color.color_count = 0
+
+    def test_build(self):
+        colored_text = textcolor('red', 'hello')
+        assert colored_text.build() == '\\textcolor{red}{hello}'
+        assert colored_text.build_preamble() == '\\usepackage[dvipsnames]{xcolor}'
+
+        colored_text = textcolor(Color(1, 0, 0), 'hello')
+        assert colored_text.build() == '\\textcolor{color1}{hello}'
+        assert colored_text.build_preamble() == cleandoc(r'''
+            \usepackage[dvipsnames]{xcolor}
+            \definecolor{color1}{rgb}{1,0,0}''')
+
+        colored_text = textcolor(Color(1, 0, 0, color_name='my_color'), 'hello')
+        assert colored_text.build() == '\\textcolor{my_color}{hello}'
+        assert colored_text.build_preamble() == cleandoc(r'''
+            \usepackage[dvipsnames]{xcolor}
+            \definecolor{my_color}{rgb}{1,0,0}''')
+
+    def test_preamble_appears_in_document(self):
+        doc = Document('test')
+        colored_text = textcolor(Color(1, 0, 0, color_name='my_color'), 'hello')
+        doc += colored_text
+        assert doc.build(False, False, False) == cleandoc(r'''
+            \documentclass{article}
+            \usepackage[utf8]{inputenc}
+            \usepackage[top=2.5cm, bottom=2.5cm, left=2.5cm, right=2.5cm]{geometry}
+            \usepackage[dvipsnames]{xcolor}
+            \definecolor{my_color}{rgb}{1,0,0}
+            \begin{document}
+            \textcolor{my_color}{hello}
+            \end{document}''')
+
+    def test_predefined_colors(self):
+        colored_text = textred('hello')
+        assert colored_text.build() == '\\textcolor{red}{hello}'
+        assert colored_text.build_preamble() == '\\usepackage[dvipsnames]{xcolor}'
+
+        colored_text = textOliveGreen('hello')
+        assert colored_text.build() == '\\textcolor{OliveGreen}{hello}'
+        assert colored_text.build_preamble() == '\\usepackage[dvipsnames]{xcolor}'
+
+    def test_textcolor_callable(self):
+        my_color_callable = textcolor_callable(Color(1, 0, 0, color_name='my_color'))
+        colored_text = my_color_callable('hello')
+        assert colored_text.build() == '\\textcolor{my_color}{hello}'
+        assert colored_text.build_preamble() == cleandoc(r'''
+            \usepackage[dvipsnames]{xcolor}
+            \definecolor{my_color}{rgb}{1,0,0}''')
